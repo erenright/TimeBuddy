cd2949598a7259c535e9cf551997cefd2623eb2c
Eric Enright <erenright@protonmail.ch>
2011-08-31 02:58:24 +0000

    Use 5am icon for report window.

--- TimeBuddy/ReportForm.Designer.cs
+++ TimeBuddy/ReportForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportForm));
             this.grid = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
@@ -51,6 +52,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 363);
             this.Controls.Add(this.grid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReportForm";
             this.Text = "TimeBuddy Report";
             this.Load += new System.EventHandler(this.ReportForm_Load);
